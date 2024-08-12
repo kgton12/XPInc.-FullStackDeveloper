ffebf01051b2da9f2f6251c162916e46ff3ec708
wellington malheiro <kgton_kgton@outlook.com>
2024-08-12 10:41:40 -0300

    feat Adds new projects and SQL scripts for database
    Added new projects to the `XPInc.-FullStackDeveloper.sln` solution file, including "SmartPhone" and "BuildingRelationalQueries".
    
    A new SQL script called `ScriptMovies.sql` was created that defines the structure of a database called `Movies`, including tables for `Actors`, `MovieCast`, `Movies`, `MovieGenres`, and `Genres`, and inserts initial data into these tables.
    
    Added several SQL queries to the `SQLMovies.sql` file to retrieve information from the `Movies` database, including searches by movie name and year, movies released in specific years, movie length, number of movies per year, and actor and genre information.

---
+++ SQLMovies.sql
@@ -0,0 +1,140 @@
+﻿
+--1 - Buscar o nome e ano dos filmes
+select
+	f.nome,
+	f.ano
+from
+	filmes f;
+
+
+--Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+select
+	f.nome,
+	f.ano
+from
+	filmes f
+order by
+	f.ano;
+
+
+--3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+select
+	f.nome,
+	f.ano,
+	f.duracao
+from
+	filmes f
+where
+	f.nome = 'De Volta para o Futuro'; 
+
+
+--4 - Buscar os filmes lançados em 1997
+select
+	f.nome,
+	f.ano,
+	f.duracao
+from
+	filmes f
+where
+	f.Ano = '1997';
+
+
+--5 - Buscar os filmes lançados APÓS o ano 2000
+select
+	f.nome,
+	f.ano,
+	f.duracao
+from
+	filmes f
+where
+	f.Ano > '2000';
+
+
+--6 - Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+select
+	f.nome,
+	f.ano,
+	f.duracao
+from
+	filmes f
+where
+	f.Duracao > '100'
+	and f.Duracao <'150'
+order by
+	f.Duracao;
+
+
+--7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+select
+	f.Ano ,
+	count(1)
+FROM
+	Filmes f
+group by
+	f.Ano
+order by
+	sum(f.Duracao) desc;
+
+
+--8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+select
+	a.primeironome,
+	a.ultimonome
+from
+	atores a
+where
+	a.Genero = 'M';
+
+
+--9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+select
+	a.primeironome,
+	a.ultimonome
+from
+	atores a
+where
+	a.Genero = 'F'
+order by
+	a.PrimeiroNome ;
+
+
+--10 - Buscar o nome do filme e o gênero
+select
+	f.Nome,
+	g.Genero
+from
+	Filmes f
+join FilmesGenero fg on
+	fg.IdFilme = f.Id
+join Generos g on
+	g.Id = fg.IdGenero;
+
+
+--11 - Buscar o nome do filme e o gênero do tipo "Mistério"
+select
+	f.Nome,
+	g.Genero
+from
+	Filmes f
+join FilmesGenero fg on
+	fg.IdFilme = f.Id
+join Generos g on
+	g.Id = fg.IdGenero
+where
+	g.Genero = 'Mistério';
+
+
+--12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+select
+	f.Nome,
+	a.PrimeiroNome,
+	a.UltimoNome,
+	ef.Papel
+from
+	Filmes f
+join ElencoFilme ef on
+	ef.IdFilme = f.Id
+join Atores a on
+	a.Id = ef.IdAtor;
+
+
