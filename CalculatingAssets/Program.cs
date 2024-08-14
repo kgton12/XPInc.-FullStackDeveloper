932033bc2d27d57b9e42686cd4bcc425a21be01e
wellington malheiro <kgton_kgton@outlook.com>
2024-08-14 10:35:15 -0300

    feat Added new .NET 8.0 projects and configurations
    - Added `MinimalApi`, `CalculatingAssets` and `VolatilityAnalysisOfAnAsset` projects to the `XPInc.-FullStackDeveloper.sln` solution.
    - Configured build and configuration properties for the new projects.
    - Created `CalculatingAssets.csproj` for .NET 8.0.
    - Implemented proportional allocations calculation in `CalculatingAssets`.
    - Added `appsettings.Development.json` and `appsettings.json` to `MinimalApi`.
    - Created `MinimalApi.csproj` with AOT support.
    - Implemented minimal API in `MinimalApi` responding to "Hello World!".
    - Added `launchSettings.json` to `MinimalApi`.
    - Implemented historical volatility calculation in `VolatilityAnalysisOfAnAsset`.
    - Created `VolatilityAnalysisOfAnAsset.csproj` for .NET 8.0.

---
+++ CalculatingAssets/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Recebe a entrada do número de ativos
+int N = int.Parse(Console.ReadLine() ?? string.Empty);
+
+// Receben e divide os valores de mercado em um array de strings
+string[] valoresMercadoStr = (Console.ReadLine() ?? string.Empty).Split(',');
+
+double[] valoresMercado = Array.ConvertAll(valoresMercadoStr, double.Parse);
+
+// Recebe o valor total investido
+double valorTotalInvestido = double.Parse(Console.ReadLine() ?? string.Empty);
+
+// Recebe e divide as alocações mínimas em um array de strings
+string[] alocacoesMinimasStr = (Console.ReadLine() ?? string.Empty).Split(',');
+
+double[] alocacoesMinimas = Array.ConvertAll(alocacoesMinimasStr, double.Parse);
+
+// Recebendo e dividindo as alocações máximas em um array de strings
+string[] alocacoesMaximasStr = (Console.ReadLine() ?? string.Empty).Split(',');
+
+double[] alocacoesMaximas = Array.ConvertAll(alocacoesMaximasStr, double.Parse);
+
+// Calcula o total do mercado
+double totalMercado = 0;
+for (int i = 0; i < N; i++)
+{
+    // TODO: Some os valores de mercado para obter o total
+    totalMercado += valoresMercado[i];
+}
+
+// Calcula as alocações proporcionais e ajustando aos limites mínimos e máximos
+double[] alocacoes = new double[N];
+for (int i = 0; i < N; i++)
+{
+    // TODO: Calcule a alocação proporcional
+    double proporcional = valoresMercado[i] / totalMercado * valorTotalInvestido;
+
+    alocacoes[i] = Math.Max(alocacoesMinimas[i], Math.Min(alocacoesMaximas[i], proporcional)); // Ajusta dentro dos limites mínimos e máximos
+}
+
+// Imprime as alocações formatadas com duas casas decimais
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"{alocacoes[i]:F2}"); // Mostra cada alocação formatada com duas casas decimais
+}
