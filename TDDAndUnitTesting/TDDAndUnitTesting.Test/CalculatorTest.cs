87f8ae89bdf2a695be0de061b6231890baa44115
wellington malheiro <kgton_kgton@outlook.com>
2024-08-21 16:44:05 -0300

    Feat : Addition of new projects and unit tests
    New projects have been added to the `XPInc.-FullStackDeveloper.sln` solution, including `TDDAndUnitTesting`, `Src`, `Test`, `TDDAndUnitTesting.Test`, and their respective project dependencies.
    
    Build platform settings and solution settings have been adjusted for the new projects.
    
    In the `CalculatorTest.cs` file, the `CalculatorTest` class has been created with test methods using `xUnit` for addition, subtraction, multiplication, and division operations, including tests for division by zero exceptions and storage of the last results.
    
    The `TDDAndUnitTesting.Test.csproj` project file has been created and configured as a test project, with references to packages such as `coverlet.collector`, `Microsoft.NET.Test.Sdk`, `xunit`, and `xunit.runner.visualstudio`.
    
    In the `Calculator.cs` file, the `Calculator` class was created with static methods for basic mathematical operations and a method to return the last three results of the operations performed.
    
    The `TDDAndUnitTesting.csproj` project file was created and configured to use the `net8.0` framework with usings and nullables enabled.

---
+++ TDDAndUnitTesting/TDDAndUnitTesting.Test/CalculatorTest.cs
@@ -0,0 +1,88 @@
+﻿namespace TDDAndUnitTesting.Test
+{
+    public class CalculatorTest
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 3, 5)]
+        public void Sum_WhenCalled_ReturnTheSumOfArguments(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            int resultSum = Calculator.Sum(a, b);
+
+            // Assert
+            Assert.Equal(resultSum, result);
+        }
+
+        [Theory]
+        [InlineData(10, 8, 2)]
+        [InlineData(5, 5, 0)]
+        public void SubTract_WhenCalled_ReturnTheSubTractOfArguments(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            int resultSubTrac = Calculator.Subtract(a, b);
+
+            // Assert
+            Assert.Equal(resultSubTrac, result);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(5, 5, 25)]
+        public void Multiply_WhenCalled_ReturnTheMultiplyOfArguments(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            int resultMultiply = Calculator.Multiply(a, b);
+
+            // Assert
+            Assert.Equal(resultMultiply, result);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(12, 2, 6)]
+        public void Divide_WhenCalled_ReturnTheDivideOfArguments(int a, int b, int result)
+        {
+            // Arrange
+
+            // Act
+            int resultDivide = Calculator.Divide(a, b);
+
+            // Assert
+            Assert.Equal(resultDivide, result);
+        }
+
+        [Fact]
+        public void TestDivisionByZero()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Throws<DivideByZeroException>(() => Calculator.Divide(10, 0));
+        }
+
+        [Fact]
+        public void TestLastResults()
+        {
+            // Arrange
+
+            // Act
+            Calculator.Subtract(10, 5);
+            Calculator.Sum(10, 5);
+            Calculator.Divide(10, 5);
+            Calculator.Multiply(10, 5);
+
+            // Assert
+            Assert.NotEmpty(Calculator.LastResults());
+            Assert.Equal(3, Calculator.LastResults().Count);
+        }
+    }
+}
---
+++ TDDAndUnitTesting/TDDAndUnitTesting/Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace TDDAndUnitTesting
+{
+    public class Calculator
+    {
+        private static readonly List<string> _lastResults = [];
+
+        public static int Sum(int a, int b)
+        {
+            _lastResults.Insert(0, $"{a} + {b} = {a + b}");
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            _lastResults.Insert(0, $"{a} - {b} = {a - b}");
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            _lastResults.Insert(0, $"{a} * {b} = {a * b}");
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            _lastResults.Insert(0, $"{a} / {b} = {a / b}");
+            return a / b;
+        }
+        public static List<string> LastResults()
+        {
+            return _lastResults.Take(3).ToList();
+        }
+    }
+}
